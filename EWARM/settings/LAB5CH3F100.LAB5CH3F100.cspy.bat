46a0647e1be8285982deb6e9916ebf1ba42ded23
MOROZOVN <oleg-1798@mail.ru>
2019-04-14 15:29:00 +0500

    1

--- EWARM/settings/LAB5CH3F100.LAB5CH3F100.cspy.bat
+++ EWARM/settings/LAB5CH3F100.LAB5CH3F100.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.2\common\bin\cspybat" -f "C:\Users\Morozov\Documents\GitHub\LAB5CH3F100\EWARM\settings\LAB5CH3F100.LAB5CH3F100.general.xcl" --backend -f "C:\Users\Morozov\Documents\GitHub\LAB5CH3F100\EWARM\settings\LAB5CH3F100.LAB5CH3F100.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.2\common\bin\cspybat" -f "C:\Users\Morozov\Documents\GitHub\LAB5_CH3_F100\EWARM\settings\LAB5CH3F100.LAB5CH3F100.general.xcl" --backend -f "C:\Users\Morozov\Documents\GitHub\LAB5_CH3_F100\EWARM\settings\LAB5CH3F100.LAB5CH3F100.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.2\common\bin\cspybat" -f "C:\Users\Morozov\Documents\GitHub\LAB5CH3F100\EWARM\settings\LAB5CH3F100.LAB5CH3F100.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\Morozov\Documents\GitHub\LAB5CH3F100\EWARM\settings\LAB5CH3F100.LAB5CH3F100.driver.xcl" 
+"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.2\common\bin\cspybat" -f "C:\Users\Morozov\Documents\GitHub\LAB5_CH3_F100\EWARM\settings\LAB5CH3F100.LAB5CH3F100.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\Morozov\Documents\GitHub\LAB5_CH3_F100\EWARM\settings\LAB5CH3F100.LAB5CH3F100.driver.xcl" 
 
 @echo off 
 :end
